aa8f1df1c2365d06a2f7ba3be83f57fb0715db7f
JoaoNetoDev <68018643+JoaoNetoDev@users.noreply.github.com>
2025-02-27 20:36:37 -0300

    Update JNP.php

--- app/service/JNP.php
+++ app/service/JNP.php
@@ -452,80 +452,6 @@ class JNP
     }
     
     
-    
-    // dump
-    public static function d(...$args)
-    {
-        $bt = debug_backtrace();
-        while(count($bt) and strpos($bt[0]['file'], __FILE__) !== false) array_shift($bt);
-        $title = ['d' => 'DUMP', 'dd' => 'DUMP AND DIE', 'de' => 'DUMP AND END'][$bt[0]['function']] . " {$bt[0]['file']}:{$bt[0]['line']}";
-        
-        ob_start();
-        foreach($args as $arg){
-            echo "<pre>";
-                $type = gettype($arg);
-                $typeName = $type; // Assume o tipo primitivo inicialmente.
-                
-                // Cálculo de tamanho para os tipos.
-                if ($type === 'object') {
-                    $typeName = get_class($arg) . ' (object)';
-                    $size = count((array)$arg);  // Conta o número de propriedades públicas do objeto.
-                } elseif ($type === 'array') {
-                    $typeName = 'Array (array)';
-                    $size = count($arg); // Conta o número de elementos no array.
-                } elseif ($type === 'string') {
-                    $typeName = 'String';
-                    $size = mb_strlen($arg); // Conta o número de caracteres na string.
-                } elseif ($type === 'integer' || $type === 'double' || $type === 'boolean' || $type === 'NULL') {
-                    // $size = serialize($arg); // Calcula o tamanho aproximado em bytes.
-                    $size = false;
-                }
-        
-                // Tratamento do output para cada tipo de dado.
-                $output = var_export($arg, true);
-                $txt_size = ($size !== false) ? "(size={$size})" : "";
-                if ($type === 'object' || $type === 'array') {
-                    $output = "{$typeName} {$txt_size}\n" . $output;
-                } else {
-                    $output = "{$typeName} {$txt_size} (" . $output . ")";
-                }
-                
-                highlight_string("<?php\n" . $output . "\n?>");
-                
-            echo "</pre>";
-        }
-        // $win = TWindow::create($title, 0.9999, 0.9999);
-        // $win->add(ob_get_clean());
-        // $win->show();
-        TSession::setValue('debugDump_string', base64_encode( ob_get_clean() ));
-        self::open_blank('debugDump', 'onShow', [] /* ['dump' => base64_encode( ob_get_clean() )] */);
-        TScript::create("__adianti_unblock_ui(); __adianti_unblock_ui();");
-    }
-    
-    // dump and die
-    public static function dd(...$args) {
-        d(...$args);
-        TScript::create("
-        
-            __adianti_unblock_ui();
-            __adianti_unblock_ui();
-        
-        ");
-        die();
-    }
-    
-    // dump and exception
-    public static function de(...$args) {
-        d(...$args);
-        TScript::create("
-        
-            __adianti_unblock_ui();
-            __adianti_unblock_ui();
-        
-        ");
-        throw new Exception("Stop");
-    }
-    
     public static function retornarNumeros($string)
     {
         // Utiliza expressão regular para manter apenas os números
@@ -664,6 +590,15 @@ class JNP
             );
         }
         return $resultado;
+    }
+
+    public static function is_date($date){
+        try {
+            new DateTime($date);
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 
     
